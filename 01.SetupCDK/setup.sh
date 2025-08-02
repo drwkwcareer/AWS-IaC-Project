673f8f1f1964e092a9a8bcf1e9600416bc23323e
drwkwcareer <drwkwcareer@gmail.com>
2025-08-02 17:02:27 +0900

    Add files via upload

---
+++ 01.SetupCDK/setup.sh
@@ -0,0 +1,39 @@
+echo "前処理（unzip, npm のインストール）を始めます。"
+sudo apt update
+sudo apt install unzip npm maven openjdk-21-jdk n -y
+sudo n 22
+
+echo "AWSCLIのインストールを始めます。"
+if ! command -v aws >/dev/null 2>&1; then
+    if [ ! -d "aws" ]; then
+        if [ ! -f "awscliv2.zip" ]; then
+            curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+        fi
+        unzip awscliv2.zip
+    fi
+    sudo ./aws/install
+else
+    echo "AWS CLIはすでにインストールされています。"
+fi
+
+echo "AWS CDKのインストールを始めます。"
+if ! command -v cdk >/dev/null 2>&1; then
+    sudo npm install -g aws-cdk
+else
+    echo "AWS CDKはすでにインストールされています。"
+fi
+
+echo "CDKプロジェクトを作成します。"
+if [ ! -d "my-java-cdk-pj" ]; then
+    mkdir my-java-cdk-pj
+    cd my-java-cdk-pj
+    cdk init app -l java
+else
+    rm -rf my-java-cdk-pj
+    mkdir my-java-cdk-pj
+    cd my-java-cdk-pj
+    cdk bootstrap
+    cdk init app -l java
+    mvn dependency:resolve
+    mvn package
+fi
