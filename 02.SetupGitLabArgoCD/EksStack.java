6a3335833585ebca346ed537166402cc93e4def6
drwkwcareer <drwkwcareer@gmail.com>
2025-08-03 22:26:55 +0900

    Update EksStack.java

--- 02.SetupGitLabArgoCD/EksStack.java
+++ 02.SetupGitLabArgoCD/EksStack.java
@@ -5,37 +5,59 @@ import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.eks.*;
-
-import java.util.List;
+import software.amazon.awscdk.services.lambda.ILayerVersion;
+import software.amazon.awscdk.services.lambda.LayerVersion;
+import software.amazon.awscdk.services.iam.*;
+import software.amazon.awscdk.Tags;
+import java.util.*;
 
 public class EksStack extends Stack {
     public final Cluster eksCluster;
 
     public EksStack(final Construct scope, final String id, final StackProps props, Vpc vpc) {
         super(scope, id, props);
+        
+        ILayerVersion kubectlLayer = LayerVersion.fromLayerVersionArn(
+            this,
+            "KubectlLayerV33",
+            "arn:aws:lambda:ap-northeast-3:386710736237:layer:kubectl:1"
+        );
 
         eksCluster = Cluster.Builder.create(this, "EKSArgoCD")
-            .version(KubernetesVersion.V1_29)
+            .clusterName("eks-cluster-argocd")
+            .version(KubernetesVersion.V1_33)
             .vpc(vpc)
+            .kubectlLayer(kubectlLayer)
             .vpcSubnets(List.of(
                 SubnetSelection.builder().subnetType(SubnetType.PRIVATE_WITH_EGRESS).build()
             ))
             .defaultCapacity(0)
             .build();
-
-        eksCluster.addFargateProfile("kube-system", FargateProfileOptions.builder()
+        
+        eksCluster.getAwsAuth().addMastersRole(Role.fromRoleArn(this, "AdminRole", "arn:aws:iam::<account>:role/<role>"));
+        
+        FargateProfile.Builder.create(this, "kube-system-fargate-profile")
+            .cluster(eksCluster)
+            .fargateProfileName("kube-system")
             .selectors(List.of(Selector.builder().namespace("kube-system").build()))
-            .subnets(List.of(
-                SubnetSelection.builder().subnetType(SubnetType.PRIVATE_WITH_EGRESS).build()
-            ))
-            .build());
+            .subnetSelection(SubnetSelection.builder().subnetType(SubnetType.PRIVATE_WITH_EGRESS).build())
+            .build();
 
-        eksCluster.addFargateProfile("argocd", FargateProfileOptions.builder()
+        FargateProfile.Builder.create(this, "argocd-fargate-profile")
+            .cluster(eksCluster)
+            .fargateProfileName("argocd")
             .selectors(List.of(Selector.builder().namespace("argocd").build()))
-            .subnets(List.of(
-                SubnetSelection.builder().subnetType(SubnetType.PRIVATE_WITH_EGRESS).build()
-            ))
-            .build());
+            .subnetSelection(SubnetSelection.builder().subnetType(SubnetType.PRIVATE_WITH_EGRESS).build())
+            .build();
+
+        for (ISubnet subnet : vpc.getPublicSubnets()) {
+            Tags.of(subnet).add("kubernetes.io/role/elb", "1");
+            Tags.of(subnet).add("kubernetes.io/cluster/eks-cluster-argocd", "shared");
+        }
+        for (ISubnet subnet : vpc.getPrivateSubnets()) {
+            Tags.of(subnet).add("kubernetes.io/role/internal-elb", "1");
+            Tags.of(subnet).add("kubernetes.io/cluster/eks-cluster-argocd", "shared");
+        }
         
     }
 }
